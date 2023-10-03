21995546abd31bf6494974c9fb37fd9a110243a2
gycsaba <gyuris.csaba@vasvari.org>
2023-10-03 19:55:38 +0200

    Az oldalnak van egy gyermek ablaka és a státuszsornak egy kezdő szövege

--- MenuProject/ViewModels/MainWindowViewModel.cs
+++ MenuProject/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,14 @@
-﻿namespace MenuProject.ViewModels
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using MenuProject.ViewModels.Base;
+
+namespace MenuProject.ViewModels
 {
-    public class MainWindowViewModel
+    public partial class MainWindowViewModel : BaseViewModel
     {
+        [ObservableProperty]
+        private BaseViewModel _childViewModel;
+
+        [ObservableProperty]
+        private string _statusBarText = "A program betölödött...";    
     }
 }
